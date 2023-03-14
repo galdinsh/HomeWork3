e526f428cdc0acc9b60bcf57847cfd1b03bed051
Vladislav <galdinsh77@mail.ru>
2023-03-14 19:24:20 +0500

    Задача №19

---
+++ Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string n = Console.ReadLine()!;
+int i =0, m = n.Length-1;
+while (i < m && n[i] == n[m])
+i++; m--;
+if (n[i] == n[m])
+Console.WriteLine($"{n} - это палиндром");
+else
+Console.WriteLine($"{n} - это не палиндром");
+
