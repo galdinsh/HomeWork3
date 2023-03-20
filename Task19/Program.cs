460a0c07f0360389eaf2f57a0fbb1c740ae2986d
Vladislav <126780307+galdinsh@users.noreply.github.com>
2023-03-20 16:40:55 +0500

    Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -3,9 +3,18 @@ Console.Write("Введите число: ");
 string n = Console.ReadLine()!;
 int i =0, m = n.Length-1;
 while (i < m && n[i] == n[m])
-i++; m--;
+{i++; m--;};
 if (n[i] == n[m])
 Console.WriteLine($"{n} - это палиндром");
 else
 Console.WriteLine($"{n} - это не палиндром");
 
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// string b = Convert.ToString(a);
+// string z = new string(b.Reverse().ToArray());
+// if (b == z)
+//     Console.WriteLine($"Число {a} - является полиндромом");
+// else
+//     Console.WriteLine($"Число {a} - не является полиндромом");
